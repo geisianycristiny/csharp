162ab5c5cd20de7733f1daa60d45f2971c4de14c
Ana Eloize <geisianycristiny7@gmail.com>
2025-02-17 11:17:19 -0400

    add exercicio3

--- exercicio1/Program.cs
+++ exercicio1/Program.cs
@@ -39,3 +39,29 @@ else
 {
     Console.WriteLine("Reprovado.");
 }
+
+
+Console.Write("Choveu? (sim/nao): ");
+string respostaChuva = Console.ReadLine().ToLower();
+bool choveu = respostaChuva == "s";
+
+    if (choveu)
+    {
+    Console.Write("Está tarde? (sim/nao): ");
+    string respostaTarde = Console.ReadLine().ToLower();
+    bool estaTarde = respostaTarde == "s";
+
+    if (estaTarde)
+     {
+          Console.WriteLine("Vou pedalar.");
+     }
+    else
+    {
+       Console.WriteLine("Vou pedalar outro dia.");
+     }
+    }
+    else
+    {
+        Console.WriteLine("Vou pedalar outro dia.");
+    }
+    
