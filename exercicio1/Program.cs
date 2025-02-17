9dd205659dbe3c287e545c6d73f6bfcfa90cf12a
Ana Eloize <geisianycristiny7@gmail.com>
2025-02-17 10:33:55 -0400

    add adicionado exercicio 1

---
+++ exercicio1/Program.cs
@@ -0,0 +1,18 @@
+﻿int idade = 16;
+bool autorizado = false;
+
+if (idade >= 18)
+{
+    Console.WriteLine("Entrada Liberada.");
+}
+else if (idade < 18)
+{
+    if (autorizado)
+    {
+        Console.WriteLine("Entrada Liberada.");
+    }
+    else
+    {
+    Console.WriteLine("Entrada nao permitida.");
+    }
+}
