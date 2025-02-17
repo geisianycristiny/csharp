d82734bb4c6841f8c60cf96c6771d36d60a95e33
Ana Eloize <geisianycristiny7@gmail.com>
2025-02-17 11:06:42 -0400

    add exercicio 2

--- exercicio1/Program.cs
+++ exercicio1/Program.cs
@@ -16,3 +16,26 @@ else if (idade < 18)
     Console.WriteLine("Entrada nao permitida.");
     }
 }
+
+
+
+Console.WriteLine("O aluno possui a presença minima? (sim/nao):");
+string presença = Console.ReadLine().ToLower();
+if(presença == "sim")
+{
+    Console.WriteLine("Digite a media do aluno:");
+    double media = double.Parse(Console.ReadLine());
+
+    if(media >= 7)
+    {
+        Console.WriteLine("Aprovado!");
+    }
+    else
+    {
+        Console.WriteLine("Reprovado!");
+    }
+}
+else 
+{
+    Console.WriteLine("Reprovado.");
+}
