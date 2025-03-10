3443b38bd5f9f4d55a38094c3a9114e31c09ab70
Ana Eloize <geisianycristiny7@gmail.com>
2025-03-10 10:04:41 -0400

    add exercicio

--- classes/exercicio-classe/Modelo/Veiculo.cs
+++ classes/exercicio-classe/Modelo/Veiculo.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
-namespace classes.Modelo
+namespace veiculos.Modelo
 {
     public class Veiculo
     {
--- classes/exercicio-classe/Program.cs
+++ classes/exercicio-classe/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using classes.Modelo;
+using veiculos.Modelo;
 
 //Criando um objeto a partir da classe Pessoa
 Veiculo obj = new Veiculo("Volkswagem", "Nivus", 2025 );
