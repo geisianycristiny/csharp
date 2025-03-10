173d9aa80ccd76b27b98ae622f652494a02e1b91
Ana Eloize <geisianycristiny7@gmail.com>
2025-03-10 08:59:52 -0400

    adicionados objetos

--- classes/Program.cs
+++ classes/Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using classes.modelo;
 
+// criando um objeto a partir da classe pessoa
+Pessoa obj = new Pessoa("Wanderson", 25,1.90,60);
+obj.Andar();
+obj.Falar();
--- classes/modelo/NewClass.cs
+++ classes/modelo/NewClass.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using classes.modelo;
 
 namespace classes.modelo
 {
